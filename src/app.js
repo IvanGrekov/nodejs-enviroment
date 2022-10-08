9f0d6a08fb6a6b2f792e2fa573021343bc8130a8
Ivan Grekov <ivan.grekov@windmill.digital>
2022-10-08 11:01:03 +0300

    testing linting of husky before commit

--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@
 import _ from 'lodash';
 
 const sum = (...numbers) => {
-    return numbers.reduce((accum, currentNumber) => accum + currentNumber, 0);
+    return numbers.reduce((accum, currentNumber) => {
+        return accum + currentNumber;
+    }, 0);
 };
 
 console.log(sum(1, 2, 3, 4, 5, 6, 7));
+
+const value = 6;
+
+// Should be an error
+if (value === 5) {
+    console.log('huray');
+}
 
 const arr1 = [1, 2, 4];
 const arr2 = [1, 2, 4];
